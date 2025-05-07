b57afaa6851e9fc89cf4ec32fa55d2de5d2d58f4
fullsack73 <liwdf3438@gmail.com>
2025-05-07 15:46:33 +0900

    added bar

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['gannet-included-jolly.ngrok-free.app']
+  }
 })
