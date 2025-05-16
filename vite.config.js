a60d0057ce8db32c4c43fd593319b39959692fb7
fullsack73 <liwdf3438@gmail.com>
2025-05-16 13:15:21 +0900

    build init

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/yt-highlight/',
   server: {
-    allowedHosts: ['gannet-included-jolly.ngrok-free.app']
+    allowedHosts: ['https://fullsack73.github.io/yt-highlight/']
   }
 })
