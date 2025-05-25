8373022ccd291760aeac8a169a396fbd91e2a885
anoBand <125958306+anoBand@users.noreply.github.com>
2025-05-25 23:48:18 +0900

    Add files via upload

---
+++ highlight extension/popup.js
@@ -0,0 +1,11 @@
+document.getElementById('getUrlBtn').addEventListener('click', async () => {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  if (tab && tab.url.includes("youtube.com/watch")) {
+    const videoUrl = tab.url;
+    const targetUrl = "https://fullsack73.github.io/yt-highlight/?videoUrl=" + encodeURIComponent(videoUrl);
+    chrome.tabs.create({ url: targetUrl });
+  } else {
+    alert("YouTube 영상 페이지가 아닙니다.");
+  }
+});
